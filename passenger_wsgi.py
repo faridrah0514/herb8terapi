5023c3fbf05cf4881940565e25de0279f4a65944
Farid Rahman <farid.rahman@grabtaxi.com>
2024-01-26 18:44:56 +0700

    modify wsgi

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -5,9 +5,12 @@ import sys
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'herb8terapi'))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'herb8terapi.settings'
 
-def application(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    message = 'It works!\n'
-    version = 'Python %s\n' % sys.version.split()[0]
-    response = '\n'.join([message, version])
-    return [response.encode()]
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+
+# def application(environ, start_response):
+#     start_response('200 OK', [('Content-Type', 'text/plain')])
+#     message = 'It works!\n'
+#     version = 'Python %s\n' % sys.version.split()[0]
+#     response = '\n'.join([message, version])
+#     return [response.encode()]
